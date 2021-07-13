7b13de4aa15180294c12130f8eaa442bcc7c3873
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2021-07-13 15:31:37 -0600

    Refactor into fullstack monorepo

--- client/src/app/dynamic/dynamic.pipe.spec.ts
+++ client/src/app/dynamic/dynamic.pipe.spec.ts
@@ -1,8 +1,9 @@
 import { DynamicPipe } from './dynamic.pipe';
+import { Injector } from '@angular/core';
 
 describe('DynamicPipe', () => {
   it('create an instance', () => {
-    const pipe = new DynamicPipe();
+    const pipe = new DynamicPipe({} as Injector);
     expect(pipe).toBeTruthy();
   });
 });
